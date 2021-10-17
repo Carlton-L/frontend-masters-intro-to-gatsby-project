79088b9df285342351248d0727d3e37d263285de
Carlton Lindsay <carlton.l@outlook.com>
2021-10-16 21:53:02 -0700

    Added links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Hello FrontendMasters!</h1>
+      <Link to="/about">About this site</Link>
     </main>
   );
 }
