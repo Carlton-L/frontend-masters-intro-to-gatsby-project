c53c04f6bb233e643456f96e01933fadc13039f5
Carlton Lindsay <carlton.l@outlook.com>
2021-10-20 18:50:19 -0700

    Added MDX support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,30 @@ module.exports = {
     description: 'Fronted Masters Intro to Gatsby course',
     image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
   },
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    // These plugins are for creating MDX blog posts from the src/posts directory
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/posts/`,
+        name: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/layout.js')
+        }
+      },
+    },
+    // next
+  ]
 }
